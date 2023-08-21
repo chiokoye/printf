6ee0937ed225954d03682077e085fd5240cb28da
Keleeze <kelechukwuezema@yahoo.com>
2023-08-21 14:07:24 -0700

    printf.c 1

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int putss(char *c);
+
+#endif
+
